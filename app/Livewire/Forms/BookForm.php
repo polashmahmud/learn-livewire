0cc3b63f6dc49d765d7b4a7621af92ac0f06d4e9
Polash Mahmud <polashmahmud@gmail.com>
2024-04-20 21:16:29 +0600

    class - 17

--- app/Livewire/Forms/BookForm.php
+++ app/Livewire/Forms/BookForm.php
@@ -2,24 +2,44 @@
 
 namespace App\Livewire\Forms;
 
+use App\Models\Book;
 use Livewire\Attributes\Rule;
 use Livewire\Attributes\Validate;
 use Livewire\Form;
 
 class BookForm extends Form
 {
+    public ?Book $book;
+
     #[Rule('required', as: 'Title of the book', message: ':attribute is required.')]
     public string $title = '';
 
     #[Rule('required', as: 'Author of the book', message: ':attribute is required.')]
     public string $author = '';
 
+    public function setBook(Book $book)
+    {
+        $this->book = $book;
+
+        $this->fill(
+            $this->book->only('title', 'author')
+        );
+    }
+
+    public function update()
+    {
+        $this->book->update(
+            $this->only('title', 'author')
+        );
+
+        $this->reset();
+    }
+
     public function create()
     {
-       auth()->user()->books()->create([
-            'title' => $this->title,
-            'author' => $this->author
-        ]);
+        auth()->user()->books()->create(
+            $this->only('title', 'author')
+        );
 
         $this->reset();
     }
--- resources/views/livewire/update-book.blade.php
+++ resources/views/livewire/update-book.blade.php
@@ -1,17 +1,17 @@
 <form wire:submit.prevent="submit" class="flex items-start space-x-3">
     <div class="grow">
         <label for="title" class="sr-only">Book Title</label>
-        <input type="text" id="title" wire:model.blur="title" placeholder="Book title"
+        <input type="text" id="title" wire:model.blur="form.title" placeholder="Book title"
                class="w-full border border-slate-300 rounded-lg">
-        @error('title')
+        @error('form.title')
         <div class="mt-1 text-sm text-red-500">{{ $message }}</div>
         @enderror
     </div>
     <div class="grow">
         <label for="author" class="sr-only">Book Author</label>
-        <input type="text" id="author" wire:model.blur="author" placeholder="Book author"
+        <input type="text" id="author" wire:model.blur="form.author" placeholder="Book author"
                class="w-full border border-slate-300 rounded-lg">
-        @error('author')
+        @error('form.author')
         <div class="mt-1 text-sm text-red-500">{{ $message }}</div>
         @enderror
     </div>
