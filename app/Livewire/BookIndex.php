ac614b5289b045c060e002eebdb92460ac5b25bc
Polash Mahmud <polashmahmud@gmail.com>
2024-05-08 21:30:23 +0600

    class - 24

--- app/Livewire/BookIndex.php
+++ app/Livewire/BookIndex.php
@@ -5,12 +5,16 @@ namespace App\Livewire;
 use App\Livewire\Forms\BookForm;
 use App\Models\Book;
 use Livewire\Attributes\On;
+use Livewire\Attributes\Url;
 use Livewire\Component;
 use Livewire\WithPagination;
 
 class BookIndex extends Component
 {
     use WithPagination;
+
+    #[Url(as: 'q', history: true)]
+    public $query = '';
 
     protected $listeners = [
         'book.create' => '$refresh'
@@ -28,7 +32,14 @@ class BookIndex extends Component
     public function render()
     {
         return view('livewire.book-index', [
-            'books' => auth()->user()->books()->latest()->paginate(3)
+            'books' => auth()->user()
+                ->books()
+                ->when(filled($this->query), function ($query) {
+                    $query->where('title', 'LIKE', '%' . $this->query . '%')
+                        ->orWhere('author', 'LIKE', '%' . $this->query . '%');
+                })
+                ->latest()
+                ->paginate(3)
         ]);
     }
 }
--- resources/views/livewire/book-index.blade.php
+++ resources/views/livewire/book-index.blade.php
@@ -1,12 +1,20 @@
 <div class="space-y-4">
-    @if($books->count())
-        <div class="space-y-2" id="books">
-            @foreach($books as $book)
-                <livewire:book-item :book="$book" wire:key="{{ $book->id }}"/>
-            @endforeach
+    <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+        <div class="p-6 text-gray-900">
+            <label for="query" class="sr-only">Search</label>
+            <input wire:model.live.debounce.300ms="query" type="search" id="query" class="w-full border border-slate-300 rounded-lg" placeholder="Search your books">
         </div>
+    </div>
+    <div class="space-y-4">
+        @if($books->count())
+            <div class="space-y-2" id="books">
+                @foreach($books as $book)
+                    <livewire:book-item :book="$book" wire:key="{{ $book->id }}"/>
+                @endforeach
+            </div>
 
-        {{ $books->links(data: ['scrollTo' => '#books']) }}
-    @endif
+            {{ $books->links(data: ['scrollTo' => '#books']) }}
+        @endif
+    </div>
 </div>
 
